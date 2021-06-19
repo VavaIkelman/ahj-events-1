6334e816062f97b6391a8fafd669ce7d77a1b1e3
VavaIkelman <mr.ilkevich@gmai.com>
2021-06-19 16:25:11 +0300

    Done eslint

--- src/js/Score.js
+++ src/js/Score.js
@@ -1,33 +1,33 @@
 export default class Score {
-    constructor(gameElement) {
-        this.gameElement = gameElement;
-        this.scoreElement = gameElement.querySelector('.result .score');
-        this.missElement = gameElement.querySelector('.result .miss');
-    }
+  constructor(gameElement) {
+    this.gameElement = gameElement;
+    this.scoreElement = gameElement.querySelector('.result .score');
+    this.missElement = gameElement.querySelector('.result .miss');
+  }
 
-    init() {
-        this.score = 0;
-        this.miss = 0;
+  init() {
+    this.score = 0;
+    this.miss = 0;
 
-        this.gameElement.addEventListener('click', (event) => {
-            const col = event.target.closest('.cage');
-            if (col) {
-                const goblin = col.querySelector('.head');
-                if (goblin) {
-                    this.hit();
-                    goblin.remove();
-                }
-            }
-        });
-    }
+    this.gameElement.addEventListener('click', (event) => {
+      const col = event.target.closest('.cage');
+      if (col) {
+        const goblin = col.querySelector('.head');
+        if (goblin) {
+          this.hit();
+          goblin.remove();
+        }
+      }
+    });
+  }
 
-    hit() {
-        this.score += 1;
-        this.scoreElement.innerText = this.score;
-    }
+  hit() {
+    this.score += 1;
+    this.scoreElement.innerText = this.score;
+  }
 
-    missed() {
-        this.miss += 1;
-        this.missElement.innerText = this.miss;
-    }
+  missed() {
+    this.miss += 1;
+    this.missElement.innerText = this.miss;
+  }
 }
